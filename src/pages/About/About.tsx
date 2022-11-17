88c6ca638b924155494915e840662ff444eca7ff
tatiana <105023527+tatianajeon@users.noreply.github.com>
2022-11-17 12:24:07 -0600

    updated with badges

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -111,6 +111,7 @@ export const About = () => {
         }}>
             Credentials
         </Typography>
+        
         <Div sx={{
             display: 'flex',
             justifyContent: 'center', 
